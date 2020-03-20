62e167a6263ec61d1e36f801d942060f9fc0803f
Jonathan Schenk <jonschenk2@gmail.com>
2020-03-20 13:05:14 -0400

    Updated methods

---
+++ FileSplitArguments.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileSplit
+{
+    public class FileSplitArguments
+    {
+        public string FileName { get; private set; }
+        public string OutputPath { get; private set; }
+        public int NumberOfLines { get; private set; }
+        public FileSplitArguments(string fileName, string outputPath, string numberOfLines)
+        {
+            FileName = fileName;
+            OutputPath = outputPath;
+            Directory.CreateDirectory(OutputPath);
+            NumberOfLines = int.Parse(numberOfLines);
+        }
+        public bool IsValid()
+        {
+            bool isValid = true;
+            if (string.IsNullOrEmpty(FileName) || !File.Exists(FileName))
+            {
+                Console.WriteLine("ERROR: Could not access input file: " + FileName);
+                isValid = false;
+            }
+            if (string.IsNullOrEmpty(OutputPath) || !Directory.Exists(OutputPath))
+            {
+                Console.WriteLine("ERROR: Could not access output folder: " + OutputPath);
+                isValid = false;
+            }
+            if (NumberOfLines == 0)
+            {
+                Console.WriteLine("ERROR: Number of lines can not be 0");
+                isValid = false;
+            }
+            return isValid;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,54 +4,29 @@ namespace FileSplit
 {
     class Program
     {
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
             if (args == null || args.Length != 3)
             {
                 PrintUsage();
-                return 1;
+                return;
             }
-            // Set Number of Lines
-            int numberOfLines;
-            try
+            var arguments = new FileSplitArguments(args[0], args[1], args[2]);
+            if (!arguments.IsValid())
             {
-                numberOfLines = int.Parse(args[2]);
-            }
-            catch
-            {
-                Console.WriteLine("ERROR: Could not convert number of lines to number: " + args[2]);
                 PrintUsage();
-                return 1;
+                return;
             }
-            if (numberOfLines == 0)
-            {
-                Console.WriteLine("ERROR: Can not set 0 as number of lines");
-                PrintUsage();
-                return 1;
-            }
-
-            // Check File Exists
-            string filepath = args[0];
-            if (!File.Exists(filepath))
-            {
-                Console.WriteLine("ERROR: Can not find file: "+ filepath);
-                return 1;
-            }
-            string filename = Path.GetFileNameWithoutExtension(filepath);
-            string extension = Path.GetExtension(filepath);
-            
-            // Check Ouput Path Exists
-            string outputpath = args[1];
-            Directory.CreateDirectory(outputpath);
-
+            string filename = Path.GetFileNameWithoutExtension(arguments.FileName);
+            string extension = Path.GetExtension(arguments.FileName);
             //Writing Files
-            using StreamReader sr = new StreamReader(filepath);
+            using StreamReader sr = new StreamReader(arguments.FileName);
             bool done = false;
             for (int fileNumber = 1; !done; fileNumber++)
             {
-                using StreamWriter sw = new StreamWriter(outputpath + filename + "-" + fileNumber + extension);
+                using StreamWriter sw = new StreamWriter(arguments.OutputPath + filename + "-" + fileNumber + extension);
                 Console.WriteLine("Writing File: " + filename + "-" + fileNumber + extension);
-                for (int lineNumber = 0; lineNumber < numberOfLines; lineNumber++)
+                for (int lineNumber = 0; lineNumber < arguments.NumberOfLines; lineNumber++)
                 {
                     string line = sr.ReadLine();
                     if (line == null)
@@ -62,9 +37,7 @@ namespace FileSplit
                     sw.WriteLine(line);
                 }
             }
-            return 0;
         }
-
         static void PrintUsage()
         {
             Console.WriteLine("Usage: FileSplit.exe <file path> <output path> <number of lines>");
